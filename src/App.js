e5dd1450f5988ee20fcbcf5f85f67553b8bb0eac
Artem <artem.ustilov@gmail.com>
2019-02-06 15:39:07 +0200

    sss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './Navbar.js'
+import Navbar from './trash/Navbar.js'
 
 class App extends Component {
   render() {
