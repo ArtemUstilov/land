fd564caf100d0f1a43aa1bbf43578a7895ca361a
BatyaIsHere <rshchibrik@gmail.com>
2019-03-23 20:04:08 +0200

    dasd

--- src/js/Services.js
+++ src/js/Services.js
@@ -39,101 +39,107 @@ class Services extends Component {
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
     }
-  componentDidMount() {
-    window.scrollTo({top: 0, behavior: "smooth"});
-  }
-    handleOpenModal () {
-        this.setState({ showModal: true });
+
+    componentDidMount() {
+        window.scrollTo({top: 0, behavior: "smooth"});
     }
 
-    handleCloseModal () {
-        this.setState({ showModal: false });
+    handleOpenModal() {
+        this.setState({showModal: true});
     }
+
+    handleCloseModal() {
+        this.setState({showModal: false});
+    }
+
     render() {
         return <div className="services">
             <section>
                 <div className="container-fluid ">
                     <div className="row d-flex justify-content-center">
                         <div className="col-md-8 col-12 no-padding">
-                <h1 className='title'><Translate id="services.title"/></h1>
-                <img src={design2} alt='' className="img-fluid"/>
-                <div className='serv-box'>
-                <h5 className="title" style={{textAlign: 'left', padding: '20px 8%'}}>OKOLITA DESIGN</h5>
-                    <div>
-                    <h4>Пакет «Перепланировка»</h4>
-               <ul>
-                   <li>Выезд на объект и обмеры помещения</li>
-                   <li>Фотофиксация объекта</li>
-                   <li>Несколько вариантов перепланировок с расстановкой мебели и сантехники</li>
-                   <li>План обмерный помещения</li>
-                   <li>Схема демонтажа</li>
-                   <li>Схема монтажа	новых простенков</li>
-                   <li>План размещения мебели/техники, сантехники</li>
-               </ul>
-                    </div>
-                    <br/>
-                    <div>
-                        <h4>Пакет «Стандарт»</h4>
-                        <ul>
-                            <li>Выезд на объект и обмеры помещения</li>
-                            <li>Фотофиксация объекта</li>
-                            <li>Несколько вариантов перепланировок с расстановкой мебели и сантехники</li>
-                            <li>План обмерный помещения</li>
-                            <li>Схема демонтажа</li>
-                            <li>Схема монтажа	новых простенков</li>
-                            <li>План размещения мебели/техники, сантехники</li>
-                            <li>План расстановки сантехнического оборудования</li>
-                            <li>План пола с учетом покрытия и учетом площади напольного покрытия</li>
-                            <li>План потолка</li>
-                            <li>Схема зон теплого пола, размещение радиаторов отопления (в зависимости от вида отопления)</li>
-                            <li>План размещения светильников и других видов осветительных приборов</li>
-                            <li>План размещения розеток</li>
-                            <li>План размещения включателей</li>
-                            <li>Развертки ванных комнат с просчетом отделочных материалов </li>
-                            <li>Развертки стен всех помещений(с учетом покрытия)</li>
-                            <li>Визуализация каждого помещения, с нескольких ракурсов</li>
-                            <li>4 выезда (на объект или в магазины для подбора или материалов)</li>
-                            <li>Объяснение чертежей строителям и консультация по техническим вопросам</li>
-                        </ul>
-                    </div>
-                    <br/>
-                    <div>
-                        <h4>Пакет «Премиум»</h4>
-                        <ul>
-                            <li>Выезд на объект и обмеры помещения</li>
-                            <li>Фотофиксация объекта</li>
-                            <li>Несколько вариантов перепланировок с расстановкой мебели и сантехники</li>
-                            <li>План обмерный помещения</li>
-                            <li>Схема демонтажа</li>
-                            <li>Схема монтажа	новых простенков</li>
-                            <li>План размещения мебели/техники, сантехники</li>
-                            <li>План расстановки сантехнического оборудования</li>
-                            <li>План пола с учетом покрытия и учетом площади напольного покрытия</li>
-                            <li>План потолка</li>
-                            <li>Схема зон теплого пола, размещение радиаторов отопления (в зависимости от вида отопления)</li>
-                            <li>План размещения светильников и других видов осветительных приборов</li>
-                            <li>План размещения розеток</li>
-                            <li>План размещения включателей</li>
-                            <li>Развертки ванных комнат с просчетом отделочных материалов </li>
-                            <li>Развертки стен всех помещений(с учетом покрытия)</li>
-                            <li>Визуализация каждого помещения, с нескольких ракурсов</li>
-                            <li>4 выезда (на объект или в магазины для подбора или материалов)</li>
-                            <li>Объяснение чертежей строителям и консультация по техническим вопросам</li>
-                            <li>Авторское сопровождение (Выезд на объект 1-2 раза в неделю, ответим на вопросы строителей при встрече или в режими онлайн (вайбер/телефону/телеграм)</li>
-                            <li>Комплектация: подбор отделочных материалов  (плитка, напольного покрытия, обои/краска/декоративная штукатурка и т.д.),
-                                корпусной мебели , мягкой мебели, светильников , сантехники, текстиля ,...)</li>
-                        </ul>
+                            <h1 className='title'><Translate id="services.title"/></h1>
+                            <img src={design2} alt='' className="img-fluid"/>
+                            <div className='serv-box'>
+                                <h5 className="title" style={{textAlign: 'left', padding: '20px 8%'}}>OKOLITA
+                                    DESIGN</h5>
+                                <div>
+                                    <h4><Translate id="services.pack1Title"/></h4>
+                                    <ul>
+                                        <li><Translate id="services.li1"/></li>
+                                        <li><Translate id="services.li2"/></li>
+                                        <li><Translate id="services.li3"/></li>
+                                        <li><Translate id="services.li4"/></li>
+                                        <li><Translate id="services.li5"/></li>
+                                        <li><Translate id="services.li6"/></li>
+                                        <li><Translate id="services.li7"/></li>
+                                    </ul>
+                                </div>
+                                <br/>
+                                <div>
+                                    <h4><Translate id="services.pack2Title"/></h4>
+                                    <ul>
+                                        <li><Translate id="services.li1"/></li>
+                                        <li><Translate id="services.li2"/></li>
+                                        <li><Translate id="services.li3"/></li>
+                                        <li><Translate id="services.li4"/></li>
+                                        <li><Translate id="services.li5"/></li>
+                                        <li><Translate id="services.li6"/></li>
+                                        <li><Translate id="services.li7"/></li>
+                                        <li><Translate id="services.li8"/></li>
+                                        <li><Translate id="services.li9"/></li>
+                                        <li><Translate id="services.li10"/></li>
+                                        <li><Translate id="services.li11"/></li>
+                                        <li><Translate id="services.li12"/></li>
+                                        <li><Translate id="services.li13"/></li>
+                                        <li><Translate id="services.li14"/></li>
+                                        <li><Translate id="services.li15"/></li>
+                                        <li><Translate id="services.li16"/></li>
+                                        <li><Translate id="services.li17"/></li>
+                                        <li><Translate id="services.li18"/></li>
+                                        <li><Translate id="services.li19"/></li>
+                                    </ul>
+                                </div>
+                                <br/>
+                                <div>
+                                    <h4>Пакет «Премиум»</h4>
+                                    <ul>
+                                        <li><Translate id="services.li1"/></li>
+                                        <li><Translate id="services.li2"/></li>
+                                        <li><Translate id="services.li3"/></li>
+                                        <li><Translate id="services.li4"/></li>
+                                        <li><Translate id="services.li5"/></li>
+                                        <li><Translate id="services.li6"/></li>
+                                        <li><Translate id="services.li7"/></li>
+                                        <li><Translate id="services.li8"/></li>
+                                        <li><Translate id="services.li9"/></li>
+                                        <li><Translate id="services.li10"/></li>
+                                        <li><Translate id="services.li11"/></li>
+                                        <li><Translate id="services.li12"/></li>
+                                        <li><Translate id="services.li13"/></li>
+                                        <li><Translate id="services.li14"/></li>
+                                        <li><Translate id="services.li15"/></li>
+                                        <li><Translate id="services.li16"/></li>
+                                        <li><Translate id="services.li17"/></li>
+                                        <li><Translate id="services.li18"/></li>
+                                        <li><Translate id="services.li19"/></li>
+                                        <li><Translate id="services.li20"/></li>
+                                        <li><Translate id="services.li21"/></li>
+                                    </ul>
 
+                                </div>
+                                <br/>
+                                <h5><Translate id="services.resultTitle"/></h5>
+                            </div>
+                        </div>
                     </div>
-                    <br/>
-                    <h5>В результате на руки вы получаете папку формата А3 с проектом</h5>
                 </div>
-                        </div></div></div>
 
             </section>
             <section id={'services-section3'}>
                 <div className="title-box ps-top-to-bottom no-padding">
-                    <h1 className='white-text title too-big white-title' style={{padding: '10px'}}><Translate id="services.from"/></h1>
+                    <h1 className='white-text title too-big white-title' style={{padding: '10px'}}><Translate
+                        id="services.from"/></h1>
                     <p className='white-text'>Запишитесь на консультацию дизайнера</p>
                     <Button green={true} onclick={this.handleOpenModal} value={'services.sign'} classes='proj-btn'/>
                     <ReactModal
@@ -152,13 +158,14 @@ class Services extends Component {
                             <br/>
                             <div className={'row'}>
                                 <div className={'col-12'}>
-                                    <input name={'phone'}  placeholder={'Ваш телефон'}/>
+                                    <input name={'phone'} placeholder={'Ваш телефон'}/>
                                 </div>
                                 <br/>
                                 <br/>
                                 <br/>
                                 <div className={'col-12'}>
-                                    <Button green={true} classes={'form-btn black-green'}value={'footer.consultation'}/>
+                                    <Button green={true} classes={'form-btn black-green'}
+                                            value={'footer.consultation'}/>
                                 </div>
                             </div>
                         </form>
@@ -169,21 +176,50 @@ class Services extends Component {
                 <div className="container-fluid ">
                     <div className="row d-flex justify-content-center">
                         <div className="col-md-8 col-12 no-padding">
-                <div className="work-box float0">
-                    <Describe num='2'
-                              text="2 березня 2006 року одностайним рішенням Національних зборів (парламенту)
-                                   Венесуели були ухвалені зміни в державній символіці, запропоновані президентом країни"
-                              icon={logo}/>
-                    <img src={logo} alt='' className="box-behind"/>
+                            <div className="work-box float1">
+                                <Describe num='1'
+                                          text={<Translate id ="services.step1"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float0">
+                                <Describe num='2'
+                                          text={<Translate id ="services.step2"/>} icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float1">
+                                <Describe num='3'
+                                          text={<Translate id ="services.step3"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float0">
+                                <Describe num='4'
+                                          text={<Translate id ="services.step4"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float1">
+                                <Describe num='5'
+                                          text={<Translate id ="services.step5"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float2">
+                                <Describe num='6'
+                                          text={<Translate id ="services.step6"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                            <div className="work-box float1">
+                                <Describe num='7'
+                                          text={<Translate id ="services.step7"/>}
+                                          icon={logo}/>
+                                <img src={logo} alt='' className="box-behind"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="work-box float1">
-                    <Describe num='3'
-                              text="Президент Венесуели Уго Чавес, представляючи нову символіку, назвав своє нововведення
-                                  «зіркою Болівара»"
-                              icon={logo}/>
-                    <img src={logo} alt='' className="box-behind"/>
-                </div>
-                        </div></div></div>
             </section>
         </div>;
     }
